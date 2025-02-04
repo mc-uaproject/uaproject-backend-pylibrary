cace8a9a342f855296da0613ce67951ef5234a3d
THEROER <theroer09@gmail.com>
2025-02-05 01:49:26 +0200

    123

--- uap_backend/config.py
+++ uap_backend/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
@@ -18,7 +18,10 @@ class Settings(BaseSettings):
     MAX_RETRIES: int = 3
     RETRY_DELAY: int = 5
 
-    model_config = SettingsConfigDict(env_file=".env", case_sensitive=True)
+    class Config:
+        env_file = ".env"
+        case_sensitive = True
+        extra=True
 
 
 settings = Settings()
