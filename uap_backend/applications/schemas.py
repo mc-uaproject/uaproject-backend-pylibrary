b8f41ee40480802545eb8a6e34322f4cb3534050
THEROER <theroer09@gmail.com>
2025-02-05 05:02:18 +0200

    123

--- uap_backend/applications/schemas.py
+++ uap_backend/applications/schemas.py
@@ -4,6 +4,8 @@ from enum import Enum
 from uap_backend.base.schemas import (
     BaseUserBackendModel,
     BothPayloadBaseModel,
+    DatetimeMixin,
+    PayloadBaseModel,
     PayloadBoth,
 )
 
@@ -16,8 +18,7 @@ class ApplicationStatus(str, Enum):
     NOT_SENT = "NOT_SENT"
 
 
-class ApplicationForm(BaseUserBackendModel):
-    status: ApplicationStatus = ApplicationStatus.NOT_SENT
+class ApplicationFormPayload(BaseUserBackendModel):
     birth_date: datetime | None
     launcher: str | None
     server_source: str | None
@@ -27,9 +28,19 @@ class ApplicationForm(BaseUserBackendModel):
     quiz_answer: str | None
 
 
+class ApplicationFormPayloadFull(PayloadBaseModel):
+    payload: ApplicationFormPayload
+
+
 class ApplicationStatusPayload(BaseUserBackendModel):
     status: ApplicationStatus
 
 
 class ApplicationStatusPayloadFull(BothPayloadBaseModel):
     payload: dict[PayloadBoth, ApplicationStatusPayload]
+
+
+class ApplicationFull(ApplicationFormPayload, ApplicationStatusPayload): ...
+
+
+class ApplicationFullMixins(ApplicationFull, DatetimeMixin): ...
