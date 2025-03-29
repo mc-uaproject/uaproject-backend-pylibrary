6000cca1706919caae631dae62bab616bdde2c75
roflboy <andriy4521@ukr.net>
2025-03-29 15:20:07 +0200

    Add PunishmentsCRUDService for managing punishment records and status updates

---
+++ uap_backend/cruds/punishments.py
@@ -0,0 +1,36 @@
+from typing import Literal
+
+from uaproject_backend_schemas.punishments import (
+    PunishmentCreate,
+    PunishmentFilterParams,
+    PunishmentResponse,
+    PunishmentStatus,
+    PunishmentUpdate,
+)
+
+from uap_backend.cruds.base import BaseCRUD
+
+
+class PunishmentsCRUDService(
+    BaseCRUD[PunishmentResponse, PunishmentCreate, PunishmentUpdate, PunishmentFilterParams]
+):
+    response_model = PunishmentResponse
+
+    def __init__(self, cache_duration=300):
+        super().__init__(cache_duration, "/punishments")
+
+    async def update_status(
+        self,
+        punishment_id: Literal["me"] | int,
+        status: PunishmentStatus,
+    ) -> PunishmentResponse:
+        """
+        Update the status of a punishment.
+        """
+        return await self._request(
+            method="POST",
+            endpoint=f"/{punishment_id}/status/{status}"
+        )
+
+
+PunishmentsCRUDServiceInit = PunishmentsCRUDService()
