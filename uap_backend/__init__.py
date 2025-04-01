e3651fea914c4c61fe2259b72e90feb5ba52a809
THEROER <theroer09@gmail.com>
2025-04-01 19:43:12 +0300

    Add exception classes to module exports for improved error handling

--- uap_backend/__init__.py
+++ uap_backend/__init__.py
@@ -1,3 +1,4 @@
 from .config import UAProjectAPISettings, settings
+from .exceptions import APIError, RequestError, ServiceError, UserDataNotFoundError
 
-__all__ = ['settings', 'UAProjectAPISettings']
+__all__ = ['settings', 'UAProjectAPISettings', 'APIError', "RequestError", "ServiceError", "UserDataNotFoundError"]
